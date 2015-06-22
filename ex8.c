a433f5efda1f1a4361dcd8e548f1c0548d5330e0
Jeffrey Paine <jeff@jeffpaine.com>
2015-06-22 11:52:02 -0400

    Add extra credit stuff.

--- ex8.c
+++ ex8.c
@@ -24,5 +24,22 @@ int main(int argc, char *argv[])
 
 	printf("name=\"%s\" and full_name=\"%s\"\n", name, full_name);
 
+	// Extra Credit
+	areas[0] = 100;
+	areas[0] = 700;
+	printf("areas[0]: %d\n", areas[0]);
+
+	name[0] = 'A';
+	printf("name: %s\n", name);
+	printf("name[0]: %c\n", name[0]);
+
+	full_name[0] = 'A';
+	printf("full_name[0]: %c\n", full_name[0]);
+
+	printf("areas[0]: %d\n", areas[0]);
+	printf("name[2]: %c\n", name[2]);
+	areas[0] = name[2];
+	printf("areas: %c\n", areas[0]);
+
 	return 0;
 }
